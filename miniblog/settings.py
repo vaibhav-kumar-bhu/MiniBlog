6f4f32a4982802c9171db6d21a62f8f6a611a04f
Vaibhava Kumar <77294073+vaibhav-kumar-bhu@users.noreply.github.com>
2021-02-26 11:30:14 +0530

    Update settings.py

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -126,7 +126,7 @@ MEDIA_URL='/media/'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'vaibhava05071994@gmail.com'
-EMAIL_HOST_PASSWORD ='9451924246'
+EMAIL_HOST_PASSWORD ='9451924****'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
